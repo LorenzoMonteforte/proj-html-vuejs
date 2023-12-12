be69ab1262dd753267833b48f01b9208da07e7b6
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2023-12-12 10:48:12 +0100

    Header (Prima parte)

---
+++ src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+    name: "Navbar",
+    props: {
+        navbarItems: Array
+    }
+}
+</script>
+
+<template>
+    <nav>
+        <ul>
+            <li v-for="navbarItem in navbarItems">
+                <strong>
+                    {{ navbarItem.title }}
+                    <font-awesome-icon class="marLef0_5rem" icon="fa-solid fa-caret-down" v-if="navbarItem.arrow == true" />
+                </strong>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<style scoped>
+ul {
+    list-style-type: none;
+}
+
+nav>ul>li:first-of-type {
+    color: rgba(233, 215, 88, 255);
+}
+
+nav>ul {
+    display: flex;
+    justify-content: space-between;
+}
+
+nav {
+    width: 100%;
+}
+
+nav>ul>li {
+    color: white;
+}
+</style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,111 @@
+<script>
+import Navbar from './Navbar.vue';
+export default {
+    name: "PageHeader",
+    components: {
+        Navbar
+    },
+    data() {
+        return {
+            navbarItems: [
+                {
+                    title: "Home",
+                    arrow: true
+                },
+                {
+                    title: "Courses",
+                    arrow: true
+                },
+                {
+                    title: "About Us",
+                    arrow: false
+                },
+                {
+                    title: "News",
+                    arrow: true
+                },
+                {
+                    title: "Pages",
+                    arrow: true
+                },
+                {
+                    title: "Contact",
+                    arrow: false
+                },
+                {
+                    title: "Purchase",
+                    arrow: false
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <div>
+            <div>
+                <img id="logo" src="../assets/img/theme_eduprime_logo.png" alt="Logo Eduprime">
+            </div>
+            <div>
+                <Navbar :navbarItems="navbarItems" />
+            </div>
+            <div>
+                <button id="btn">
+                    <strong>VIEW COURSES</strong>
+                </button>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+header {
+    height: 1000px;
+    background-color: red;
+    padding-top: 3rem;
+}
+
+header>div:first-child {
+    display: flex;
+    padding: 0 3rem;
+}
+
+header>div:first-child>div:first-child,
+header>div:first-child>div:last-child {
+    width: 25%;
+}
+
+header>div:first-child>div:nth-child(2) {
+    width: 50%;
+    display: flex;
+    align-items: center;
+    padding: 0 1.5rem;
+}
+
+#logo {
+    width: 100%;
+}
+
+header>div:first-child>div:first-child {
+    padding-right: 1.5rem;
+    display: flex;
+    align-items: center;
+}
+
+header>div:first-child>div:last-child {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+}
+
+#btn {
+    font-size: 1rem;
+    padding: 1.5rem 3rem;
+    border: none;
+    border-radius: 2rem;
+    color: white;
+    background-color: rgba(233, 215, 88, 255);
+}
+</style>
